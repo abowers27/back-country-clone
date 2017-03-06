d4ed69c00204deff68c2d23dbee5aa53c7afde3d
Andrew Bowers <andeb27@gmail.com>
2017-03-06 10:25:29 -0700

    gitignore

--- server/massive.js
+++
@@ -1,5 +0,0 @@
-var massive = require('massive');
-var connectionString = "postgres://postgres:postgres@localhost/back-country";
-// var connectionString = 'postgres://bokvtqru:nI7-9twmr52TNM5HMMbBR0ISoBU7Y7i-@babar.elephantsql.com:5432/bokvtqru'
-var massiveInstance = massive.connectSync({connectionString : connectionString});
-module.exports = massiveInstance;
