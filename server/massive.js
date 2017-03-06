e7d03c3ee92e09234345fa5bcf3474eba3f1e8de
Andrew Bowers <andeb27@gmail.com>
2017-03-06 09:56:57 -0700

    gs

--- server/massive.js
+++ server/massive.js
@@ -1,4 +1,5 @@
 var massive = require('massive');
 var connectionString = "postgres://postgres:postgres@localhost/back-country";
+// var connectionString = 'postgres://bokvtqru:nI7-9twmr52TNM5HMMbBR0ISoBU7Y7i-@babar.elephantsql.com:5432/bokvtqru'
 var massiveInstance = massive.connectSync({connectionString : connectionString});
 module.exports = massiveInstance;
